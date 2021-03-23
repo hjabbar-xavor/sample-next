3c81c7490fc7375eb37c2eab15028c0e8188aeb4
ondrejch <Simply007@users.noreply.github.com>
2021-03-23 13:34:44 +0100

    add information about secret

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -2,7 +2,7 @@ export default async function preview(req, res) {
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
   if (
-    req.query.secret !== process.env.KONTENT_PREVIEW_SECRET
+    req.query.secret !== process.env.PREVIEW_SECRET
   ) {
     return res
       .status(401)
